eb4f2d5150e033cb8eb0dba3f84bd9c401307e11
Vincent Duchêne <duchene.vincent@gmail.com>
2012-11-02 01:16:00 +0100

    work on suggestion view

--- sources/prototype/Web/db/seeds.rb
+++ sources/prototype/Web/db/seeds.rb
@@ -59,7 +59,7 @@ User.create([
 
 
 Video.create([
-  { :thumbnail_url => "http://fanart.tv/fanart/tv/80379/seasonthumb/B_80379%20(3).jpg", :type_video => "Serie", :title => " La fluctuation de l'ouvre-boîte électrique", :year => 2009, :duration => 1380, :country => "USA", :video_url => "http://vps.vincex86.be/videos/bbt/3/bbts03E02/bbts03E01.ism/manifest", :bo_url => "http://www.wat.tv/video/the-big-bang-theory-bande-1upid_2exzl_.html", :synopsis => "Sheldon rentre au Texas en disgrâce quand il apprend que ses camarades ont altéré les données de son expédition dans l'Arctique...", :position => 2, :tagline => "humour#rire#sheldon#leaonard#tbbt#big#bang", :season => 3, :star_rating_avg => 2.8,
+  { :thumbnail_url => "http://fanart.tv/fanart/tv/80379/seasonthumb/B_80379%20(3).jpg", :type_video => "Serie", :title => "La fluctuation de l'ouvre-boîte électrique", :year => 2009, :duration => 1380, :country => "USA", :video_url => "http://vps.vincex86.be/videos/bbt/3/bbts03E02/bbts03E01.ism/manifest", :bo_url => "http://www.wat.tv/video/the-big-bang-theory-bande-1upid_2exzl_.html", :synopsis => "Sheldon rentre au Texas en disgrâce quand il apprend que ses camarades ont altéré les données de son expédition dans l'Arctique...", :position => 1, :tagline => "humour#rire#sheldon#leaonard#tbbt#big#bang", :season => 3, :star_rating_avg => 2.8,
   :created_at => nil, :updated_at => nil }],
   :without_protection => true )
 
@@ -69,6 +69,6 @@ Video.create([
     :without_protection => true )
 
 Video.create([
-  { :thumbnail_url => "http://fanart.tv/fanart/tv/80379/seasonthumb/B_80379%20(3).jpg", :type_video => "Serie", :title => "La déviation Gotowitz", :year => 2009, :duration => 1380, :country => "USA", :video_url => "http://vps.vincex86.be/videos/bbt/3/bbts03E02/bbts03E03.ism/manifest", :bo_url => "http://www.wat.tv/video/the-big-bang-theory-bande-1upid_2exzl_.html", :synopsis => "Sheldon tente de faire retrouver à Penny sa bonne humeur avec des chocolats afin qu'elle soit plus gentille envers lui. Raj et Howard décident de s'habiller façon «gothique» afin d'aller draguer dans un night club.", :position => 2, :tagline => "humour#rire#sheldon#leaonard#tbbt#big#bang", :season => 3, :star_rating_avg => 3.8,
+  { :thumbnail_url => "http://fanart.tv/fanart/tv/80379/seasonthumb/B_80379%20(3).jpg", :type_video => "Serie", :title => "La déviation Gotowitz", :year => 2009, :duration => 1380, :country => "USA", :video_url => "http://vps.vincex86.be/videos/bbt/3/bbts03E02/bbts03E03.ism/manifest", :bo_url => "http://www.wat.tv/video/the-big-bang-theory-bande-1upid_2exzl_.html", :synopsis => "Sheldon tente de faire retrouver à Penny sa bonne humeur avec des chocolats afin qu'elle soit plus gentille envers lui. Raj et Howard décident de s'habiller façon «gothique» afin d'aller draguer dans un night club.", :position => 3, :tagline => "humour#rire#sheldon#leaonard#tbbt#big#bang", :season => 3, :star_rating_avg => 3.8,
   :created_at => nil, :updated_at => nil }],
   :without_protection => true )

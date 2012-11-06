045ca61b326e93ac2ba28f3c2946ec3f39dad9c2
Vincent Duchêne <duchene.vincent@gmail.com>
2012-11-06 23:02:30 +0100

    update suggestion view when clip loaded

--- sources/prototype/Web/app/assets/javascripts/suggestions.js
+++ sources/prototype/Web/app/assets/javascripts/suggestions.js
@@ -1,14 +1,15 @@
 function UpdateCurrentVideo(id) {
-    var oldElement = document.getElementsByClassName("class");
+    var oldElement = document.getElementsByClassName("active");
     for(var i=0; i<oldElement.length; i++)
     {
+        if(oldElement[i].id.indexOf("s")== 0)
+        {
+            oldElement[i].setAttribute("class", "inactive");
+        }
 
     }
 
     var element = document.getElementById("s" + id);
     element.setAttribute("class", "active");
-
-
-
 
 }
